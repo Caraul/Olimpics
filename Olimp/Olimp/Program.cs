2a282fe8d06d16f1a0251ca614235d7b9ee872ce
Bobafett89 <kolya10042006@gmail.com>
2019-11-24 17:22:30 +0300

    new

--- Olimp/Olimp/Program.cs
+++ Olimp/Olimp/Program.cs
@@ -4,82 +4,88 @@ namespace Olimp
 {
     class Program
     {
+        static int i = 0, v = 0, x = 0, l = 0, c = 0, d = 0, m = 0, num; // объявил здесь поскольку, эти переменные используются в обоих методах, а если их объявлять в методах то они используются только в этих методах, а int поскольку var в одной строке сразу много переменных объявлять нельзя  
         static void Main(string[] args)
         {
-            int i = 0, v = 0, x = 0, l = 0, c = 0, d = 0, m = 0, num = 0; // оставил для того, что если буду переделывать в другой день
-            for (int cycle = 101; cycle < 201; cycle++, num = ((cycle + cycle * cycle) / 2) % 2048)
+            for (var index = 101; index < 201; index++)
             {
+                num = ((index + index * index) / 2) % 2048;
                 while (num != 0)
                 {
-                    if (num >= 1000)
-                    {
-                        num -= 1000;
-                        m++;
-                    }
-                    else if (num >= 500)
-                    {
-                        num -= 500;
-                        d++;
-                    }
-                    else if (num >= 400)
-                    {
-                        num -= 400;
-                        c++;
-                        d++;
-                    }
-                    else if (num >= 100)
-                    {
-                        num -= 100;
-                        c++;
-                    }
-                    else if (num >= 90)
-                    {
-                        num -= 90;
-                        x++;
-                        c++;
-                    }
-                    else if (num >= 50)
-                    {
-                        num -= 50;
-                        l++;
-                    }
-                    else if (num >= 40)
-                    {
-                        num -= 40;
-                        x++;
-                        l++;
-                    }
-                    else if (num >= 10)
-                    {
-                        num -= 10;
-                        x++;
-                    }
-                    else if (num == 9)
-                    {
-                        num -= 9;
-                        x++;
-                        i++;
-                    }
-                    else if (num >= 5)
-                    {
-                        num -= 5;
-                        v++;
-                    }
-                    else if (num == 4)
-                    {
-                        num -= 4;
-                        i++;
-                        v++;
-                    }
-                    else if (num >= 1)
-                    {
-                        num -= 1;
-                        i++;
-                    }
+                    count();
                 }
             }
-            Console.WriteLine("D");
-            Console.WriteLine(d);
+            Console.WriteLine("M");
+            Console.WriteLine(m);
+        }
+        static void count()
+        {
+             
+            if (num >= 1000)
+            {
+                num -= 1000;
+                m++;
+            }
+            else if (num >= 500)
+            {
+                num -= 500;
+                d++;
+            }
+            else if (num >= 400)
+            {
+                num -= 400;
+                c++;
+                d++;
+            }
+            else if (num >= 100)
+            {
+                num -= 100;
+                c++;
+            }
+            else if (num >= 90)
+            {
+                num -= 90;
+                x++;
+                c++;
+            }
+            else if (num >= 50)
+            {
+                num -= 50;
+                l++;
+            }
+            else if (num >= 40)
+            {
+                num -= 40;
+                x++;
+                l++;
+            }
+            else if (num >= 10)
+            {
+                num -= 10;
+                x++;
+            }
+            else if (num == 9)
+            {
+                num -= 9;
+                x++;
+                i++;
+            }
+            else if (num >= 5)
+            {
+                num -= 5;
+                v++;
+            }
+            else if (num == 4)
+            {
+                num -= 4;
+                i++;
+                v++;
+            }
+            else if (num >= 1)
+            {
+                num -= 1;
+                i++;
+            }
         }
     }
 }
