e3401024a2a0ba77e283c987ea1f23c202b2b54a
Bobafett89 <kolya10042006@gmail.com>
2019-12-19 23:32:22 +0300

    new program

---
+++ Olimp/Progarm2Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Olimp2;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void YearExceptionTest()
+        {
+           // Assert.ThrowsException<System.Exception>(() => Program2.FirstDayOfYear(1));
+        }
+        [TestMethod]
+        public void program()
+        {
+            //int ExpectedMonth = 1, ExpectedDay = 4;
+            //Program2.prog(2018, 1);
+            //Assert.IsTrue(Program2.month == ExpectedMonth && Program2.DayOfMonth == ExpectedDay);
+        }
+    }
+}
